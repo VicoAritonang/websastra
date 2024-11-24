384a3d4637f044ef92be97dfdc070fad4ef8afa4
VicoAritonang <chaikaaaaa5@gmail.com>
2024-11-24 14:27:55 +0700

    wsgi fix

--- websastra/wsgi.py
+++ websastra/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'handler.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'websastra.settings')
 
 application = get_wsgi_application()
 
