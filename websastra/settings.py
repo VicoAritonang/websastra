f09c99825b429fb3d87f15b7d4aa65bea3295a99
VicoAritonang <chaikaaaaa5@gmail.com>
2024-11-24 14:06:38 +0700

    second

--- websastra/settings.py
+++ websastra/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^4i=94olh2+!%j2$t3bs)l4=8699ya+7o^woaz9kd*+cvvfnev
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".vercel.app", "www.websastra.com"]
 
 
 # Application definition
